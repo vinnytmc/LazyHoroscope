8bfdbf30664219fd6cd6b98ece1c8329c84dbc85
vinnytmv <valopez@mail.csuchico.edu>
2018-09-19 18:00:04 -0700

    Allows user to determine there lucky number

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
 string firstName;
 string lastName;
+int lucky;
   //get user input
 cout<<"enter in your first name\n";
 cin>>firstName;
@@ -16,5 +17,6 @@ cout<<"enter in your last name\n";
 cin>>lastName;
   //tell fortune
 cout<<"welcome "<<firstName[0]<<lastName[0]<<" here is your fortune \n";
+cout<<"your lucky number is "<<firstName.length()<<"\n";
  return 0;
 }
