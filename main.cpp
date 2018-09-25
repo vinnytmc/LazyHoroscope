b06e38b4e11905a70551f76fc22da9b7899105b6
vinnytmv <valopez@mail.csuchico.edu>
2018-09-24 22:49:09 -0700

    Enables user to determine lucky number based on there name

--- main.cpp
+++ main.cpp
@@ -7,16 +7,41 @@ using namespace std;
 int main()
 {
   //declare variables
-string firstName;
-string lastName;
-int lucky;
+  string firstName;
+  string lastName;
+  int lucky;
+  int index;
+  int index2;
+
   //get user input
-cout<<"enter in your first name\n";
-cin>>firstName;
-cout<<"enter in your last name\n";
-cin>>lastName;
+  cout<<"enter in your first name\n";
+  cin>>firstName;
+  cout<<"enter in your last name\n";
+  cin>>lastName;
   //tell fortune
-cout<<"welcome "<<firstName[0]<<lastName[0]<<" here is your fortune \n";
-cout<<"your lucky number is "<<firstName.length()<<"\n";
- return 0;
+  index = firstName.length()-1;
+  index2 = lastName.length()-1;
+
+  cout<<"welcome "<<firstName[0]<<lastName[0]<<" here is your fortune \n";
+  cout<<"your lucky number is "<<firstName.length()<<"\n";
+
+  if(firstName[0] == 'a' || firstName[0] == 'A' || firstName[0] == 'e' || firstName[0] == 'E' || firstName[0] == 'i' || firstName[0] == 'I' || firstName[0] == 'o' || firstName[0] == 'O' || firstName[0] == 'u' || firstName[0] == 'U')
+  {
+    cout<<"your destine to be famous\n";
+  }
+  else 
+  {
+    cout<<"keep a low profile\n";
+  }
+
+  if(lastName[index2] == 'a' || lastName[index2] == 'A' || lastName[index2] == 'e'|| lastName[index2] == 'E' || lastName[index2] == 'i' || lastName[index2] == 'I'|| lastName[index2] == 'o' || lastName[index2] == 'O' || lastName[index2] == 'u'|| lastName[index2] == 'U')
+  {
+    cout<<"you have already met your true love\n";
+  }
+  cout<<"Have a good day!\n";
+
+
+  return 0;
+
 }
+
