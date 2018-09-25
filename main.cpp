00cfe969420382badedd94c2f58a18ce38980a4f
vinnytmv <valopez@mail.csuchico.edu>
2018-09-24 23:06:49 -0700

    Enables user to determine lucky number based on there name

--- main.cpp
+++ main.cpp
@@ -14,20 +14,20 @@ int main()
   int index2;
 
   //get user input
-  cout<<"what is in your first name\n";
+  cout<<"what is in your first name?\n";
   cin>>firstName;
-  cout<<"what in your last name\n";
+  cout<<"what in your last name?\n";
   cin>>lastName;
   //tell fortune
   index = firstName.length()-1;
   index2 = lastName.length()-1;
 
-  cout<<"welcome "<<firstName[0]<<lastName[0]<<" here is your fortune \n";
+  cout<<"welcome, "<<firstName[0]<<"."<<lastName[0]<<".,"<<" here is your fortune...\n";
   cout<<"your lucky number is "<<firstName.length()<<"\n";
 
   if(firstName[0] == 'a' || firstName[0] == 'A' || firstName[0] == 'e' || firstName[0] == 'E' || firstName[0] == 'i' || firstName[0] == 'I' || firstName[0] == 'o' || firstName[0] == 'O' || firstName[0] == 'u' || firstName[0] == 'U')
   {
-    cout<<"your destine to be famous\n";
+    cout<<"your destined to be famous!\n";
   }
   else 
   {
@@ -36,7 +36,7 @@ int main()
 
   if(lastName[index2] == 'a' || lastName[index2] == 'A' || lastName[index2] == 'e'|| lastName[index2] == 'E' || lastName[index2] == 'i' || lastName[index2] == 'I'|| lastName[index2] == 'o' || lastName[index2] == 'O' || lastName[index2] == 'u'|| lastName[index2] == 'U')
   {
-    cout<<"you have already met your true love\n";
+    cout<<"you have already met your true love.\n";
   }
   cout<<"have a good day!\n";
 
