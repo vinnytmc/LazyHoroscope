7fea8a10d50bcce536a6aa3aa3676ed6b81cc01d
vinnytmv <valopez@mail.csuchico.edu>
2018-09-24 23:11:17 -0700

    Enables user to determine lucky number based on there name

--- main.cpp
+++ main.cpp
@@ -14,20 +14,20 @@ int main()
   int index2;
 
   //get user input
-  cout<<"what is in your first name?\n";
+  cout<<"what is your first name?\n";
   cin>>firstName;
-  cout<<"what in your last name?\n";
+  cout<<"what is your last name?\n";
   cin>>lastName;
   //tell fortune
   index = firstName.length()-1;
   index2 = lastName.length()-1;
 
-  cout<<"welcome, "<<firstName[0]<<"."<<lastName[0]<<".,"<<" here is your fortune...\n";
+  cout<<"Welcome, "<<firstName[0]<<"."<<lastName[0]<<".,"<<" here is your fortune...\n";
   cout<<"your lucky number is "<<firstName.length()<<"\n";
 
   if(firstName[0] == 'a' || firstName[0] == 'A' || firstName[0] == 'e' || firstName[0] == 'E' || firstName[0] == 'i' || firstName[0] == 'I' || firstName[0] == 'o' || firstName[0] == 'O' || firstName[0] == 'u' || firstName[0] == 'U')
   {
-    cout<<"your destined to be famous!\n";
+    cout<<"you are destined to be famous!\n";
   }
   else 
   {
